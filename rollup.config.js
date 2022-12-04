8541e10190065759c1eabfca859e10d6e7722d37
Chason <cai981012@163.com>
2022-12-04 20:05:11 +0800

    feat: init

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+export default {
+  input: './index.js',
+  output: {
+    file: 'output.js',
+    format: 'cjs',
+    strict: true,
+    // banner: '#! /usr/bin/env node\n',
+  },
+  plugins: []
+}
